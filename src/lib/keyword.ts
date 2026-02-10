0fc16431828cecff7a36de0a57a45d11f87ac6ef
Rekabriyan <rekabriyanch08@gmail.com>
2026-02-10 09:33:53 +0700

    feat: add keywords and meta information for SEO enhancement

---
+++ src/lib/keyword.ts
@@ -0,0 +1,16 @@
+export const KEYWORDS = [
+    "mochi ice cream",
+    "es krim mochi",
+    "mochi ice cream bandung",
+    "dessert mochi",
+    "smoochi mochi",
+    "reseller mochi",
+    "supplier mochi",
+    "gofood mochi",
+    "grabfood mochi",
+    "shopeefood mochi",
+    "jual mochi bandung",
+    "jual mochi di bandung",
+    "mochi ice cream bandung",
+  ];
+  
---
+++ src/lib/meta.ts
@@ -0,0 +1,8 @@
+import { KEYWORDS } from "./keyword";
+
+export const HOME_META = {
+  title: "Smoochi Mochi Ice Cream Bandung",
+  description:
+    "Smoochi Mochi Ice Cream Bandung adalah dessert mochi lembut dengan isian es krim premium. Tersedia untuk reseller & pemesanan online.",
+  keywords: KEYWORDS.join(", "),
+};
