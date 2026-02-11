f7aaf852402cda75fb3a3f437f433fb436ae76a8
Paradox281 <kharismatio220@gmail.com>
2026-02-11 08:49:38 +0700

    Fixbug Keyword can not be read

--- src/lib/helmet.tsx
+++ src/lib/helmet.tsx
@@ -1,17 +1,31 @@
+import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 
 type Props = {
   title: string;
   description: string;
   keywords?: string;
+  structuredData?: object;
 };
 
-export default function SEO({ title, description, keywords }: Props) {
+export default function SEO({ title, description, keywords, structuredData }: Props) {
+  useEffect(() => {
+    const staticKeywords = document.querySelector('meta[name="keywords"]:not([data-rh="true"])');
+    if (staticKeywords) {
+      staticKeywords.remove();
+    }
+  }, []);
+
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       {keywords && <meta name="keywords" content={keywords} />}
+      {structuredData && (
+        <script type="application/ld+json">
+          {JSON.stringify(structuredData)}
+        </script>
+      )}
     </Helmet>
   );
 }
